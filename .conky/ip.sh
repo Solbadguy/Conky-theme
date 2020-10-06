cc9de217039d0b53b1a4ec19360784573a7439a0
Solbadguy <solbadguy90210@gmail.com>
2020-10-06 17:32:22 +0300

    Update ip.sh
    noticed constant requests to the service, added a pause.

--- .conky/ip.sh
+++ .conky/ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # eigene öffentliche ip anzeigen
 
-wget http://checkip.dyndns.org/ -q -O - |
+wget http://checkip.dyndns.org/ -q -O --wait=60 - |
 grep -Eo '\<[[:digit:]]{1,3}(\.[[:digit:]]{1,3}){3}\>'
